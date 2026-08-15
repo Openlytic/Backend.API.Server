76a291a0d4c8240497797eda657c916cbefc1ec1
Md. Zihad <md.zihad0246@gmail.com>
2026-08-16 04:01:52 +0600

    feat(routing, server): add organization routes and rate limiting for organization requests

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 
+import { organizationRouter } from 'src/modules/organization/organization.router'
 import { userRouter } from 'src/modules/user/user.router'
 
 const router = Router()
@@ -11,6 +12,9 @@ router.get('/health', (req, res) => {
 
 // Auth Routes
 router.use('/auth', userRouter)
+
+// Organization Routes
+router.use('/organization', organizationRouter)
 
 export const restRoutes = router
 export const routes = router
