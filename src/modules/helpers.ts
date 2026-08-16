4e95dc4a33be33d77606cf9c8d5dea22cfc183e7
Md. Zihad <md.zihad0246@gmail.com>
2026-08-17 02:59:29 +0600

    feat(sqs-client): add SQS client for publishing messages with fallback logic

--- src/modules/helpers.ts
+++ src/modules/helpers.ts
@@ -1,3 +1,6 @@
 // Barrel: helper namespaces, populated per feature branch.
+export * as appQueueHelper from 'src/modules/app-queue/app-queue.helper'
 export * as commonHelper from 'src/modules/common/common.helper'
+export * as emailRecipientHelper from 'src/modules/email-recipient/email-recipient.helper'
+export * as emailHelper from 'src/modules/email/email.helper'
 export * as organizationHelper from 'src/modules/organization/organization.helper'
--- src/modules/services.ts
+++ src/modules/services.ts
@@ -1,3 +1,6 @@
 // Barrel: service namespaces, populated per feature branch.
+export * as appQueueService from 'src/modules/app-queue/app-queue.service'
 export * as authService from 'src/modules/auth/auth.service'
+export * as emailRecipientService from 'src/modules/email-recipient/email-recipient.service'
+export * as emailService from 'src/modules/email/email.service'
 export * as organizationService from 'src/modules/organization/organization.service'
